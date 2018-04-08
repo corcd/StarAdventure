16ec2d31fd15f6f621eddd1b722bd14717b25575
王含泽 <whzcorcd@gmail.com>
2018-04-09 01:17:13 +0800

    内容模块更新

--- WebHook/Content.php
+++ WebHook/Content.php
@@ -2,20 +2,29 @@
     require("Info.php");
     $fl = file_get_contents("php://input");
     $jsonObj = json_decode($fl, true);
+
     // Parser Aligenie Skill JSON
     $intentName = $jsonObj['intentName'];
     $utterance = $jsonObj['utterance'];
-    $original = "";
+    $originalValue = "";
     foreach($jsonObj['slotEntities'] as $k=>$v){
-        if ($v['intentParameterName']=== 'content'){
-            $original = $v['originalValue'];
-            //$reply = $original;
+        if ($v['intentParameterName'] === 'content'){
+            $originalValue = $v['originalValue'];
             break;
         }
     }
-    $reply = "hello!";
-    // Content Nexus
 
+    // Content Nexus
+    if($originalValue === "英语"){
+        $reply = "让我们开始今天的英语课程；你可以学习单词跟读、字母认知";
+    }
+    else if($originalValue === "语文"){
+        $reply = "让我们开始今天的语文课程；你可以学习古诗词、作文、拼音";
+    }
+    else if($originalValue === "数学"){
+        $reply = "让我们开始今天的数学课程；你可以学习加减法、乘法口诀";
+    }
+    
 	// Echo Result for Aligenie
     $resultObj->returnCode = "0";
     $resultObj->returnErrorSolution = "";
