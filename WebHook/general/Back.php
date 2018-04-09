112630532d8988ccb5dcdc4743cd6c68d2d593e8
王含泽 <whzcorcd@gmail.com>
2018-04-09 20:11:39 +0800

    增加通用模块

---
+++ WebHook/general/Back.php
@@ -0,0 +1,30 @@
+<?php
+    $fl = file_get_contents("php://input");
+    $jsonObj = json_decode($fl, true);
+
+    // Parser Aligenie Skill JSON
+    $intentName = $jsonObj['intentName'];
+    $utterance = $jsonObj['utterance'];
+    $originalValue = "";
+    foreach($jsonObj['slotEntities'] as $k=>$v){
+        if ($v['intentParameterName'] === 'page'){
+            $originalValue = $v['originalValue'];
+            break;
+        }
+    }
+
+    // Content Nexus
+    $reply = "好的，这就为你返回到".$originalValue;
+
+	// Echo Result to Aligenie
+    $resultObj->returnCode = "0";
+    $resultObj->returnErrorSolution = "";
+    $resultObj->returnMessage = "";
+        $returnValue->reply= $reply;
+        $returnValue->resultType= "RESULT";
+        $resultValue->executeCode="SUCCESS";
+        $resultValue->msgInfo="";
+    $resultObj->returnValue=$returnValue;
+    $resultJSON = json_encode($resultObj);
+    echo $resultJSON;
+?>
