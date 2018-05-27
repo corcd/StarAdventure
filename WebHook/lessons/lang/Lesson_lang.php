871e8cac67e1c37f6311ea2f11f5f49f7bc2a226
王含泽 <whzcorcd@gmail.com>
2018-05-27 17:18:53 +0800

    学习内容中删除前置逻辑判断模块

--- WebHook/lessons/lang/Lesson_lang.php
+++ WebHook/lessons/lang/Lesson_lang.php
@@ -1,38 +1,17 @@
 <?php
-    require("../function.php");
     $fl = file_get_contents("php://input");
-    $lastintentName = file_get_contents('../../Info/LastIntent.mem');
-    if($lastintentName === '课程选择'){
-        //return true;
-    }
-    else{
-        $resultObj->returnCode = "0";
-        $resultObj->returnErrorSolution = "";
-        $resultObj->returnMessage = "";
-        $returnValue->reply= "不好意思哈，当前的阶段不提供这个功能呢";
-        $returnValue->resultType= "CONFIRM";
-        $resultValue->executeCode="SUCCESS";
-        $resultValue->msgInfo="";
-        $resultObj->returnValue=$returnValue;
-        $resultJSON = json_encode($resultObj);
-        echo $resultJSON;
-        //return false;
-        exit(0);
-    }
     $jsonObj = json_decode($fl, true);
-    //file_put_contents('./json.txt', print_r($jsonObj,true));
-    //file_put_contents('./json_0.txt', print_r($jsonObj['slotEntities'][0]['intentParameterName'],true));
 
     // Parser Aligenie Skill JSON
     $intentName = $jsonObj['intentName'];
         file_put_contents('../../Info/LastIntent.mem', $intentName);  //Output The Lastest Intent Name
     $utterance = $jsonObj['utterance'];
     $intentId = $jsonObj['intentId'];
-    $originalValue_content = "";
-    $originalValue_action = "";
+    $Value_content = "";
+    $Value_action = "";
     // foreach($jsonObj['slotEntities'] as $k=>$v){
     //     if ($v['intentParameterName'] === 'lang_content'){
-    //         $originalValue = $v['originalValue'];
+    //         $Value = $v['Value'];
     //         break;
     //     }
     // }
@@ -43,19 +22,19 @@
 
     if ($jsonObj['slotEntities'][0]['intentParameterName'] === 'lang_content'){
         //$temp['intentParameterName'] = $v['intentParameterName'];
-        //$temp['originalValue'] = $v['originalValue'];
-        $temp['lang_content'] = $jsonObj['slotEntities'][0]['originalValue'];
-        $originalValue_content = $jsonObj['slotEntities'][0]['originalValue'];
+        //$temp['Value'] = $v['Value'];
+        $temp['lang_content'] = $jsonObj['slotEntities'][0]['standardValue'];
+        $Value_content = $jsonObj['slotEntities'][0]['standardValue'];
         //$temp['standardValue'] = $v['standardValue'];
         //$temp['liveTime'] = $v['liveTime'];
         //$temp['createTimeStamp'] = $v['createTimeStamp'];
         }
     if ($jsonObj['slotEntities'][1]['intentParameterName'] === 'poem_action'){
-        $temp['poem_action'] = $jsonObj['slotEntities'][1]['originalValue'];
-        $originalValue_action = $jsonObj['slotEntities'][1]['originalValue'];
+        $temp['poem_action'] = $jsonObj['slotEntities'][1]['standardValue'];
+        $Value_action = $jsonObj['slotEntities'][1]['standardValue'];
     }
     //file_put_contents('./log.txt', print_r($temp,true));
-    $reply = $originalValue_content."".$originalValue_action;
+    $reply = $Value_content."".$Value_action;
     $index = "";
     $poem_name = "";
     $article_name = "";
@@ -63,9 +42,9 @@
     $desc = "";
 
     // Content Nexus
-    if($originalValue_action === "无"){
+    if($Value_action === "无"){
         //
-        switch ($originalValue_content) {
+        switch ($Value_content) {
             case "古诗词": 
                 //$index = rand(1,3);
                 $index = 2;
@@ -121,10 +100,10 @@
             case "作文": $desc ="";break;
         }
     }
-    else if($originalValue_action === "介绍"){
+    else if($Value_action === "介绍"){
        $reply = "这首诗的背景是";
     }
-    else if($originalValue_action === "解释"){
+    else if($Value_action === "解释"){
        $reply = "这首诗讲述了";
     }
 
@@ -144,6 +123,6 @@
         $resultValue->msgInfo="";
     $resultObj->returnValue=$returnValue;
     $resultJSON = json_encode($resultObj);
-    file_put_contents('./replyJson.txt', print_r($resultObj,true));
+    //file_put_contents('./replyJson.txt', print_r($resultObj,true));
     echo $resultJSON;
 ?>
--- WebHook/lessons/lang/Lesson_poem.php
+++ WebHook/lessons/lang/Lesson_poem.php
@@ -1,46 +1,28 @@
 <?php
     $fl = file_get_contents("php://input");
-    $lastintentName = file_get_contents('../../Info/LastIntent.mem');
-    if($lastintentName === '语文星球'){
-        //return true;
-    }
-    else{
-        $resultObj->returnCode = "0";
-        $resultObj->returnErrorSolution = "";
-        $resultObj->returnMessage = "";
-        $returnValue->reply= "不好意思哈，当前的阶段不提供这个功能呢";
-        $returnValue->resultType= "CONFIRM";
-        $resultValue->executeCode="SUCCESS";
-        $resultValue->msgInfo="";
-        $resultObj->returnValue=$returnValue;
-        $resultJSON = json_encode($resultObj);
-        echo $resultJSON;
-        //return false;
-        exit(0);
-    }
     $jsonObj = json_decode($fl, true);
 
     // Parser Aligenie Skill JSON
     $intentName = $jsonObj['intentName'];
     $utterance = $jsonObj['utterance'];
-    $originalValue = "";
+    $Value = "";
     foreach($jsonObj['slotEntities'] as $k=>$v){
         if ($v['intentParameterName'] === 'action'){
-            $originalValue = $v['originalValue'];
+            $Value = $v['standardValue'];
             break;
         }
     }
     $reply = "";
 
     // Content Nexus
-    if($originalValue == "介绍"){
+    if($Value == "介绍"){
         $reply = "《枫桥夜泊》是唐朝安史之乱后，诗人张继途经寒山寺时，写下的一首羁旅诗。在这首诗中，诗人精确而细腻地讲述了一个客船夜泊者对江南深秋夜景的观察和感受，勾画了月落乌啼、霜天寒夜、江枫渔火、孤舟客子等景象，有景有情有声有色。此外，这首诗也将作者羁旅之思，家国之忧，以及身处乱世尚无归宿的顾虑充分地表现出来，是写愁的代表作。
         这首诗句句形象鲜明，可感可画，句与句之间逻辑关系又非常清晰合理，内容晓畅易解。不仅是中国历代各种唐诗选本和别集选入此诗，连亚洲一些国家的小学课本也曾收录此诗。寒山寺也因此诗的广为传诵而成为游览胜地。";
     }
-    else if($originalValue == "解释"){
+    else if($Value == "解释"){
         $reply = "这首诗句句形象鲜明，可感可画，句与句之间逻辑关系又非常清晰合理，内容晓畅易解。不仅是中国历代各种唐诗选本和别集选入此诗，连亚洲一些国家的小学课本也曾收录此诗。寒山寺也因此诗的广为传诵而成为游览胜地。";
     }
-    else if($originalValue == "作者"){
+    else if($Value == "作者"){
         $reply = "张继，字懿孙，汉族，湖北襄州（今湖北襄阳）人。唐代诗人，生平事迹不详，约公元753年前后在世，与刘长卿为同时代人。据诸家记录，仅知他是约天宝十二年（约公元七五三年）的进士。大历中，以检校祠部员外郎为洪州（今江西南昌市）盐铁判官。他的诗爽朗激越，不事雕琢，比兴幽深，事理双切，对后世颇有影响。但可惜流传下来的不到50首。他的最著名的诗是《枫桥夜泊》。";
     }
 
